220ae20cfd7bde966366c79cedeb39906e2ea11c
Mahasin Prodhan <mahasinprodhan2@gmail.com>
2025-10-31 23:20:28 +0530

    I changed the headding

--- client/src/pages/admin/Comments.jsx
+++ client/src/pages/admin/Comments.jsx
@@ -1,7 +1,11 @@
 import React, { useEffect, useState } from "react";
+
 import { comments_data } from "../../assets/assets";
+
 import CommentTableItem from "../../components/admin/CommentTableItem";
+
 import { useAppContext } from "../../context/AppContext";
+
 import { toast } from "sonner";
 
 const Comments = () => {
